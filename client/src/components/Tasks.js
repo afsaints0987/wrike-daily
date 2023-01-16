be84cad738406716f6ab21f71a990da380fc23e3
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-01-16 18:35:29 +0800

    update client

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -3,14 +3,14 @@ import axios from 'axios'
 
 const Tasks = ({ accessToken, contactId }) => {
     const [tasks, setTasks] = useState()
-    const newTask = 'IEADVMHZJMBRONOE'
+    // const newTask = 'IEADVMHZJMBRONOE'
     const date = new Intl.DateTimeFormat("fr-CA", { year: "numeric", month: "2-digit", day: "2-digit" }).format(Date.now())
 
     useEffect(() => {
         const getTasks = async () => {
             if (accessToken) {
 
-                const response = await axios.get(`https://www.wrike.com/api/v4/tasks?responsibles=['${contactId}']&scheduledDate=${date}&customStatuses=[${newTask}]`, {
+                const response = await axios.get(`https://www.wrike.com/api/v4/tasks?responsibles=['${contactId}']&scheduledDate=${date}`, {
                     headers: {
                         Authorization: `Bearer ${accessToken}`
                     }
