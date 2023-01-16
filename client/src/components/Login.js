c59056f677c3fde54e53004e11bbd839d73ce4d8
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-01-16 17:41:19 +0800

    update login function

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,15 +1,23 @@
 import Button from 'react-bootstrap/Button'
 import logo from '../assets/Wrike_logo_2020.png'
 import Loading from '../components/Loading'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { UserContext } from '../context/UserContext';
+import { useNavigate } from 'react-router-dom'
 
 function Login(img) {
     const [isLoading, setIsLoading] = useState(false)
-
+    const [cookies, setCookie] = useContext(UserContext)
+    const navigate = useNavigate()
+    
     const handleLogin = () => {
         setIsLoading(true)
         
         window.location.replace('https://wrike-daily.vercel.app/login')
+
+        if(cookies.data){
+            navigate('/dashboard')
+        }
     }
 
     if (isLoading) {
