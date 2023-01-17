6218682ab5f4d33971686882853946e8fd720dfa
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-01-17 15:21:17 +0800

    update index.js and vercel.json

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+    res.sendFile(path.join(__dirname, 'client/build','index.html'));
 });
 
 
