a6141637f6fac2406853be603f28428a1cd5adce
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-01-17 13:23:36 +0800

    update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(express.urlencoded({extended: false}))
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 })
 
 app.use('/', require('./routes/LoginRoute'))
