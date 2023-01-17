f0685e1067e35b8f57b15fe7278d7e5bfd9f51b7
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-01-17 15:37:59 +0800

    update index.js and vercel.json

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { UserAuthProvider } from './context/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserAuthProvider>
-    <Router basename={process.env.PUBLIC_URL}>
-      <App />
-    </Router>
+      <Router>
+        <App />
+      </Router>
     </UserAuthProvider>
   </React.StrictMode>
 );
--- index.js
+++ index.js
@@ -13,13 +13,13 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client', 'public')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build','index.html'));
+    res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
 });
 
 
