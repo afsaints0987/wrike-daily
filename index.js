192d0b5a48ad5436c2a45ccfd6b9a2de27ad7fae
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-01-17 14:28:45 +0800

    update index.js

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 
