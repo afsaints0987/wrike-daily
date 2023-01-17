2812b8382ec25d16c63538c19014b87c9c6d900d
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-01-17 14:19:14 +0800

    update index.js and vercel.json

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-    // res.send('Authorization Granted')
-})
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
 
 app.use('/', require('./routes/LoginRoute'))
 
