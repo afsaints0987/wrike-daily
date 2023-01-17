41335dda863b3ec45cc8bb0af28faefafec8427b
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-01-17 15:53:05 +0800

    update index.js manifest.json

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+app.use('/api', require('./routes/LoginRoute'))
 
-app.use(express.static(path.join(__dirname, 'client', 'public')));
+app.use(express.static(path.join(__dirname, 'client/public')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/public', 'index.html'));
 });
 
 
-app.use('/api', require('./routes/LoginRoute'))
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`)
