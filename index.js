417d0dee6cbfee7421cf8554259b5d54cf72f72d
Aaron Fritz Santos <Aaron.Santos@EntainGroup.com>
2023-01-17 12:07:29 +0800

    update login route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -14,8 +15,10 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+app.use(express.static("public"))
+
 app.get('/', (req, res) => {
-    res.send('Authorization Successful')
+    res.sendFile(path.join(__dirname, "public", "index.html"))
 })
 
 app.use('/', require('./routes/LoginRoute'))
